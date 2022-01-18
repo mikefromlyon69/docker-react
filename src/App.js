7d8f91d0ac1349a48596848d822530c67e97ed64
Mickael <jazzkeats@gmail.com>
2022-01-18 11:01:26 +0100

    Correction App.js

--- src/App.js
+++ src/App.js
@@ -7,8 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. Ok, doing it! Again here dqo
-       d </p>
+          Edit <code>src/App.js</code> and save to reload. Ok, doing it!  </p>
         <a
           className="App-link"
           href="https://reactjs.org"
