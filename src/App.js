a6a6e3f7e82b02dcf0c657beadeeee3d5d027ba0
Mickael <jazzkeats@gmail.com>
2022-01-18 13:11:27 +0100

    change App.js test for travis

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. Ok, doing it!  </p>
+          Edit <code>src/App.js</code> and save to reload. Ok, doing it! Travis deploy  </p>
         <a
           className="App-link"
           href="https://reactjs.org"
